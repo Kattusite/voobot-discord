9877d9fae5ff7bf1df750f5ebfd69b79f8430d9b
Kattusite <Kattusite@users.noreply.github.com>
2021-02-16 01:32:29 -0500

    first pass tinydb

--- voobot/emojistats.py
+++ voobot/emojistats.py
@@ -1,12 +1,14 @@
 import discord
 from discord.ext import commands
-import yaml
+
+import tinydb
+from tinydb import operations as dbops
 
 import asyncio
 import datetime
 from collections import defaultdict
-import glob
-import os
+from operator import itemgetter
+from os import path as op
 
 import logging
 logger = logging.getLogger(__name__)
@@ -15,161 +17,142 @@ CACHE_DIR = 'cache'
 
 # How far back do we expect to find changes to entries already in the cache?
 # The older of N messages ago, or T days ago.
-REFETCH_LAST_N = 200
-REFETCH_SINCE_DAYS = 7
+RESCAN_LAST_N = 200
+RESCAN_SINCE_DAYS = 7
 
-class MessageData():
-    def __init__(self, id: str, date: str, time: str, author: str, channel: str):
-        self.id = id
-        self.datetime = datetime.datetime.strptime(f'{date} {time}', '%Y-%m-%d %H:%M:%S.%f')
-        self.author = author
-        self.channel = channel
+DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S.%f'
 
-class ReactData():
-    def __init__(self, emoji: str, sender: str, msg_id: str, msg_date: str,
-                 msg_time: str, msg_author: str, channel: str):
-        self.emoji = emoji
-        self.sender = sender
-        self.msg = MessageData(msg_id, msg_date, msg_time, msg_author, channel)
+def dttos(dt):
+    """ Convert the provided datetime to a string. """
+    return dt.strftime(DATETIME_FORMAT)
+
+def stodt(s):
+    """ Convert the provided string to a datetime. """
+    return datetime.datetime.strptime(s, DATETIME_FORMAT)
 
 class EmojiStats(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-        self.react_data = defaultdict(lambda: [])
-        """ Maps channel name to List[ReactData] """
+        # This assumes the bot will only ever run on one server.
+        self.db = tinydb.TinyDB(op.join(CACHE_DIR,'cache.json'),
+                                encoding='utf-8',
+                                indent=2,
+                                ensure_ascii=False)
 
-        # Read emoji history.
-
-        # If the cache file exists, read from it
-        # I expect entries to look like this:
-        #   time,channel,emoji,sender,receiver
-
-        # Search through available channels to find new messages
-        # (those sent since the most recent message in cache)
-        # NOTE: This assumes no reacts are added to old messages.
-        #       Perhaps do min(1 week, oldest cached)
-        # Something like:
-        #           always check last 100 msgs, and messages newer than 24h
-        #           but read all other results from cache
-
-        # TODO: This also doesn't include messages that are themselves emoji, or that include emoji
-        # These can be:
-        #   time,channel,emoji,sender,
-
-    def load_cache_file(self, filename):
-        """ Load the cache file of the given name.
-            Update self.react_data to reflect the new data.
-        """
-        if not os.path.isfile(filename):
-            logger.warning(f'cache file {filename} does not exist!')
-            return
-
-        with open(filename, 'r', encoding='utf-8') as f:
-            data = yaml.load(f)
-
-        # Strip off the '.yaml'
-        channel = filename[:-5]
-
-        # Flatten each record so we can filter more easily
-        # Yes, this is janky.
-        rds = []
-        for ymd, records in data.items():
-            for r in records:
-                for emoji, users in r['emoji'].items():
-                    for u in users:
-                        rd = ReactData(emoji=emoji,
-                                       sender=u,
-                                       msg_id=r['msg'],
-                                       msg_date=ymd,
-                                       msg_time=r['time'],
-                                       msg_author=r['author'],
-                                       channel=channel)
-                        rds.append(rd)
-
-        # WARNING HUGE BUG:
-        # This does not handle merges correctly.
-        # react_data needs to be a map of channel -> message-id -> ReactData, so
-        # we can overwrite existing messages easily
-        # NAH FUCK IT
-        # TINYDB ALL THE WAY
-        # STOP FUCKING HAND ROLLING DATABASES
-        # YIKES
-
-
-        # Already existing entries will need to be updated
-        self.react_data[channel] += rds
-
-    async def load_cache(self):
-        """ Read the yaml files in the cache directory into memory, and use
-            the data to populate self.react_data.
-        """
-
-        cache_filenames = glob.glob(os.path.join(CACHE_DIR, '*.yaml'))
-
-        coros = [asyncio.to_thread(self.load_cache_file, fn) for fn in filename]
-        await asyncio.gather(*coros)
+        self.cache = self.db.table('message_cache')
+        self.sentinels = self.db.table('channel_sentinels')
 
     async def rescan_channel(self, ctx, channel):
-        """ Rescan a given channel [TODO: or just a recent chunk of it] to
-            populate its cache file with all reactions.
+        """ Rescan a given channel (or just a recent chunk of it) to
+            populate self.cache with all sent messages with emoji or reacts.
+
+            TODO: This doesn't yet include messages that are themselves emoji,
+                  or that include emoji in the body
         """
 
         if not channel.permissions_for(ctx.guild.me).read_message_history:
             logger.warning(f'Bot not permitted to read_message_history in {channel.name}')
             return
 
-        if not self.react_data:
-            await self.load_cache()
+        # Figure out how far back we'd like to invalidate the cache.
+        # This will be the older of:
+        # 1) RESCAN_SINCE_DAYS days ago, OR
+        # 2) the datetime of the RESCAN_LAST_N'th most recent message in that channel.
+            # BUG: This currently checks the Nth most recent message WITH A REACTION.
+            #      That is wildly different than Nth most recent, which is a problem...
+        # 2b) TODO: change it to: RESCAN_SINCE_DAYS before the last seen channel sentry.
+        # TODO: This fails to account for the case where a channel has many many messages
+        #       and almost no emoji, as we end up rescanning the entire list every time.
+        #       This defeats the purpose of the cache :/
+        # IDK what to do abt that other than just add sentries to the db.
+        # e.g. db.insert({channel_sentry: channel_id, last_seen: datetime})
 
-        limit = None
+        Message = tinydb.Query()
 
-        # If we've got some cache for this channel, find the Nth oldest message
-        if channel.name in self.react_data:
-
-
-
-
-        # TODO: Do some magic to figure out which history is "recent enough"
-
-
-        records = defaultdict(lambda: [])
-        """ A collection of records that look like this:
-            yyyy-mm-dd: [
-                {
-                    "msg": msgid,
-                    "author": userid,
-                    "time": hh:mm:ss.f
-                    "emoji": {
-                        "emojistr": [userid, userid, userid],
-                        "emojistr": [userid, userid, userid],
-                    }
-                },
-            ]
+        # New alg to fix all the bugs I had before
         """
+            1.  Get sentinel from cache. timeA = sentinel.datetime.
+                If no result, return None to rescan all.
+                By definition, timeA is the most recent time for which we might
+                possibly have any information, invalidated or otherwise.
+            2.  Get last RESCAN_LAST_N messages before timeA, oldest first.
+            3.  timeB = msg.datetime. Don't continue iterating.
+            4.  timeC = timeA - timedelta(days=RESCAN_SINCE_DAYS)
+            5.  Return min(timeB, timeC).
+        """
+
+        async def get_cache_invalidation_time():
+            """ Determine how far back we'd like to invalidate the cache.
+
+                TODO: Explain why this algorithm works, and why we need to invalidate the cache at all.
+                        (to avoid dirty reads if reacts have been added since last scan)
+                    Avoids the case of a channel with many messages, few reacts.
+                    Assumes no "necro" reacting of very old messages, unless
+                    those messages are the most recent in their channel.
+
+                Returns:
+                    - datetime.datetime, the time before which cache entries are
+                      accepted as-is, and after which they are to be rescanned
+                      from Discord.
+            """
+            # 1. Fetch sentinel time for this channel from cache.
+            # This is the most recent datetime for which we have any data cached,
+            # valid or otherwise.
+            sentinel = self.sentinels.search(Message.channel == channel.id)
+            if not sentinel:
+                return None
+            sentinel_time = stodt(sentinel['datetime'])
+
+            # 2. Get time of the message RESCAN_LAST_N before the sentinel.
+            async for msg in channel.history(limit=None, before=sentinel_time, oldest_first=True):
+                timeA = msg.created_at
+                break
+
+            # 3.  Get time exactly RESCAN_SINCE_DAYS before the sentinel.
+            timeB = sentinel_time - datetime.timedelta(days=RESCAN_SINCE_DAYS)
+
+            # 4. Use whichever of the two times is earlier to minimize the
+            #    risk of dirty cache entries.
+            return min(timeA, timeB)
+
+        rescan_since = await get_cache_invalidation_time()
+
         async def insert_record(msg):
-            ymd = msg.created_at.strftime("%Y-%m-%d")
-            records[ymd].append({
-                "msg": msg.id,
-                "author": msg.author.id,
-                "time": msg.created_at.strftime("%H:%M:%S.%f"),
-                "reacts": {str(r): [user.id for user in await r.users().flatten()] for r in msg.reactions}
-            })
+            """ Insert a `Message` record into the cache. """
+            self.cache.upsert({
+                'datetime': msg.created_at.strftime('%Y-%m-%d %H:%M:%S.%f'),
+                'id':       msg.id,
+                'channel':  channel.id,
+                'author':   msg.author.id
+            }, Message.id == msg.id)
+
+            # Forcibly overwrite reacts to ensure deleted reacts are removed from cache.
+            reacts = {str(r): [user.id for user in await r.users().flatten()] for r in msg.reactions}
+            self.cache.update(dbops.set('reacts', reacts), Message.id == msg.id)
 
         # Find all the reacts in the last `limit` messages
-        logger.info(f'enumerating channel history for {channel.name}')
-        async for msg in channel.history(limit=limit):
+        since_str = rescan_since.strftime("%Y-%m-%d %H:%M:%S.%f") if rescan_since else "forever"
+        logger.info(f'Scanning channel history: {channel.name} since {since_str}')
+        newest_msg = None
+        async for msg in channel.history(limit=None, after=rescan_since, oldest_first=True):
             if msg.reactions:
                 await insert_record(msg)
+            newest_msg = msg
 
-        # This might slow the f out of the main thread, maybe use asyncio.to_thread()
-        filename = os.path.join(CACHE_DIR, f'{channel.name}.yaml')
-        logger.info(f'dumping {channel.name} to cache...')
-        with open(filename, 'w', encoding='utf-8') as f:
-            yaml.dump(dict(records), f, allow_unicode=True)
-        logger.info(f'{channel.name} dump complete!')
+        # Store the newest message we've scanned in this channel as the sentinel.
+        if newest_msg:
+            # TODO: Searching for sentinels in the massive haystack of cache
+            #       is needlessly slow. Just make a second table in the db,
+            #       exclusively for sentinel records.
+            #       For that matter, move messages to a table with a more apropos name than _default.
+            # Time it first just to see what the perf impact really is..
+            self.sentinels.upsert({
+                'datetime': dttos(newest_msg.created_at),
+                'channel':  channel.id
+            }, Message.channel == channel.id)
 
-        # await self.send_emoji_table(ctx, emojis, limit)
+        logger.info(f'{channel.name} scan complete!')
 
     @commands.command()
     async def rescan(self, ctx):
